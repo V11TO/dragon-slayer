436f3f41b26da5c5498aa4cd15c0949260ca8657
V11TO <145458104+V11TO@users.noreply.github.com>
2025-08-26 11:37:24 -0300

    lk

--- script.js
+++ script.js
@@ -6,57 +6,108 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Você é Kael Drakar, um guerreiro de passado sombrio, conhecido por caçar as criaturas mais perigosas dos céus. Um dragão ancestral conhecido como Vharzagul, o Devorador de Céus, despertou nas Montanhas Escaldantes. O rei implora sua ajuda.Nas muralhas do castelo, o velho mago Halric se aproxima de você.oferecendo encantos antigos.",
+        enunciado: "Em um Reino muito, muito e muito distante, havia um guerreiro(Você). Certa vez, ele reccebeu uma missão para destruir uma criatura que foi avistada em uma montanha nos arredores. Você se prepara para sair do castelo ",
         alternativas: [
-            "Aceitar os feitiços de proteção, mesmo que isso signifique dever um favor ao mago.",
-            "Recusar a ajuda, acreditando que apenas sua espada e coragem são suficientes.",
+            {
+                texto: "Aceita a ajuda do mago Halric",
+                afirmacao: "Você carrega consigo runas de proteção."
+            },
+            {
+                texto: "Parte sozinho",
+                afirmacao: "Sua lâmina brilha com confiança inabalável."
+            }
         ]
     },
     {
-        enunciado: "Você cavalga pelas terras selvagens. Ao cair da tarde, chega a uma encruzilhada. À esquerda, o Desfiladeiro dos Ecos, onde muitos guerreiros desapareceram. À direita, a Floresta dos Cantos, lar de elfos caçadores.",
+        enunciado: "Na estrada, você encontra uma bifurcação.",
         alternativas: [
-            "Arriscar o desfiladeiro, para chegar mais rápido à montanha.",
-            "Seguir pela floresta, talvez encontrando aliados... ou armadilhas.",
+            {
+                texto: "Segue pelo desfiladeiro",
+                afirmacao: "Você ganha tempo, mas sente os olhos de inimigos ocultos."
+            },
+            {
+                texto: "Vai pela floresta",
+                afirmacao: "Você conquista aliados que prometem lutar ao seu lado."
+            }
         ]
     },
     {
-        enunciado: "Em meio às colinas, você encontra Telmere em ruínas — homens queimados, crianças chorando. O dragão passou por ali. Um grupo de sobreviventes te suplica por ajuda.",
+        enunciado: "Ao chegar a um vilarejo destruído pelo fogo, vê sobreviventes pedindo ajuda.",
         alternativas: [
-            "Proteger os sobreviventes e levá-los a um abrigo.",
-            "Ignorar os pedidos e seguir o rastro quente do dragão.",
+            {
+                texto: "Socorre os aldeões",
+                afirmacao: "Corações agradecidos confiam em você."
+            },
+            {
+                texto: "Ignora os pedidos",
+                afirmacao: "Você avança, guiado apenas pela sede de caçada."
+            }
         ]
     },
     {
-        enunciado: "Você alcança a base da Montanha Escaldante. A trilha está coberta por cinzas e rochas instáveis. Ruge o trovão — ou seria o dragão?",
+        enunciado: "Na montanha do dragão, a tempestade ruge.",
         alternativas: [
-            "Esperar a neblina baixar e recuperar energia.",
-            "Subir durante a tormenta, aproveitando que o dragão pode estar distraído.",
+            {
+                texto: "Espera a tormenta passar",
+                afirmacao: "Sua mente se fortalece com paciência estratégica."
+            },
+            {
+                texto: "Escala durante a tempestade",
+                afirmacao: "Sua coragem o impulsiona, apesar do risco mortal."
+            }
         ]
     },
     {
-        enunciado: "Você entra em uma câmara colossal esculpida por fogo e lava. Vharzagul dorme sobre uma pilha de ossos e ouro antigo. Ele está vulnerável… mas há runas seladoras gravadas em pilares ao redor.",
+        enunciado: "Dentro do covil, o dragão adormece sobre tesouros.",
         alternativas: [
-           "Usar as runas e magia para selar o dragão eternamente.",
-            "Despertar a fera e enfrentá-la em um combate direto e mortal.",
+            {
+                texto: "Tenta selá-lo com magia",
+                afirmacao: "Você escolhe o caminho da sabedoria e do equilíbrio."
+            },
+            {
+                texto: "Ataca de frente",
+                afirmacao: "Você escolhe o caminho da fúria e da glória sangrenta."
+            }
         ]
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
